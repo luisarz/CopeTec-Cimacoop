0aa51c387ef46e30e1d3bdc2b5874fa86628eabf
wom987 <wom987@hotmail.com>
2023-07-25 22:27:27 -0600

    feat: add foreing keys

--- CopeTec-Cimacoop/database/migrations/2023_07_25_201454_create_declaracion_juradas_table.php
+++ CopeTec-Cimacoop/database/migrations/2023_07_25_201454_create_declaracion_juradas_table.php
@@ -12,11 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('declaracion_juradas', function (Blueprint $table) {
-            $table->id();
+            $table->id('declaracion_id');
             $table->string('origen_fondos');
             $table->string('comprobante_procedencia_fondo');
             $table->string('lugar');
-            $table->date('fecha');
+            $table->date('fecha')
+            $table->unsignedBigInteger('id_cliente');
+            $table->foreign('id_cliente')->references('id_cliente')->on('clientes');
+            $table->unsignedBigInteger('id_cuenta');
+            $table->foreign('id_cuenta')->references('id_cuenta')->on('clientes');
             $table->timestamps();
         });
     }
