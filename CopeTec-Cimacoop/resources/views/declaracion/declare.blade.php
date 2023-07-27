ff8b957284cb589e45ea3337f0e51701424696ea
wom987 <wom987@hotmail.com>
2023-07-26 21:45:51 -0600

    feat: add form layouts

--- CopeTec-Cimacoop/resources/views/declaracion/declare.blade.php
+++ CopeTec-Cimacoop/resources/views/declaracion/declare.blade.php
@@ -12,7 +12,7 @@
 
             <div class="ribbon-label fs-3">
                 <i class="ki-outline  {{ Session::get('icon_menu') }}  text-white fs-2x"></i> &nbsp;
-                Agregar Declaración | {{ Session::get('name_module') }}
+                FORMULARIO- DECLARACION JURADA - CUENTAS DE AHORRO | {{ Session::get('name_module') }}
                 <span class="ribbon-inner bg-info"></span>
             </div>
         </div>
@@ -27,8 +27,80 @@
                     </ul>
                 </div>
             @endif
-                
 
+            <form>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h5 class="my-4 text-center">
+                                Datos Generales del cliente (Persona natural) o representante legal (Persona Jurídica)
+                            </h5>
+                        </div>
+                        {{-- Client name --}}
+                        <div class="col-5">NOMBRE DEL ASOCIADOO REPRESENTANTE LEGAL</div>
+                        <div class="col-7">Client name</div>
+                        {{-- ID number --}}
+                        <div class="col-5">Número de documento (DUI, PASAPORTE, CARNÉ DE RESIDENTE)</div>
+                        <div class="col-7">Id Number</div>
+                        <div class="col-12">
+                            <h5 class="my-4 text-center">
+                                Datos Generales de la cuenta de ahorro
+                            </h5>
+                        </div>
+                        {{-- start savings section --}}
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Número de depositos</label>
+                                <input type="number" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Valor promedio de depósitos</label>
+                                <input type="number" step="0.00" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Al depositar realizare abonos mediante:</label>
+                                <select class="form-control custom-select custom-select-lg mb-3">
+                                    <option value="Ambos">Ambos (Cheque y/o Efectivo)</option>
+                                    <option value="Cheque">Cheque</option>
+                                    <option value="Efectivo">Efectivo</option>
+                                </select>
+                            </div>
+                        </div>
+                        {{-- start withdrawals --}}
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Número de retiros</label>
+                                <input type="number" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Valor promedio de retiros</label>
+                                <input type="number" step="0.00" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Al realizar retiros lo ha hare mediante:</label>
+                                <select class="form-control custom-select custom-select-lg mb-3">
+                                    <option value="Ambos">Ambos (Cheque y/o Efectivo)</option>
+                                    <option value="Cheque">Cheque</option>
+                                    <option value="Efectivo">Efectivo</option>
+                                </select>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </form>
         </div>
 
     </div>
