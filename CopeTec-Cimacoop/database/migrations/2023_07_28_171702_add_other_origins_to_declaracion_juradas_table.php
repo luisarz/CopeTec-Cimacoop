42bbc300d5b67144af777c2d7c4907edb29661ea
wom987 <wom987@hotmail.com>
2023-07-28 17:18:57 -0600

    feat: add other origins migrations to table

---
+++ CopeTec-Cimacoop/database/migrations/2023_07_28_171702_add_other_origins_to_declaracion_juradas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('declaracion_juradas', function (Blueprint $table) {
+            $table->boolean('otro_origen_fondos')->nullable();
+            $table->boolean('otro_comprobante_fondos')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('declaracion_juradas', function (Blueprint $table) {
+            $table->dropColumn('otro_origen_fondos');
+            $table->dropColumn('otro_origen_fondos');
+        });
+    }
+};
