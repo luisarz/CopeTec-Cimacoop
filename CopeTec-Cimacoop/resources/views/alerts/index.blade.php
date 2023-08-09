566012c4c26621e7bacf64e850fd23d50715d8c6
wom987 <45610462+wom987@users.noreply.github.com>
2023-08-08 23:01:23 -0600

    feat: add margins

--- CopeTec-Cimacoop/resources/views/alerts/index.blade.php
+++ CopeTec-Cimacoop/resources/views/alerts/index.blade.php
@@ -29,56 +29,58 @@
             @endif
 
         </div>
-        <div class="table-responsive">
-            <table class="data-table-coop table table-hover table-row-dashed fs-6     gy-2 gs-5">
-                <thead class="thead-dark">
-                    <tr class="fw-semibold fs-3 text-gray-800 border-bottom-2 border-gray-200">
-                        <th class="min-w-150px">Acciones</th>
-                        <th>Id Cuenta</th>
-                        <th>Cliente</th>
-                        <th>Monto transacción (WIP)</th>
-                        <th>Caja (WIP)</th>
-                        <th>Atendido por: (WIP)</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($notifications as $n)
-                        <tr>
-                            <td>
-                                <div class="d-flex">
-                                    @if (!$n->read_at)
-                                        <a n="javascript:void(0);" tol-tip="Marcar como leída" data-offset="20px 20px"
-                                            data-toggle="popover" data-placement="top" data-content="Example content"
-                                            class="btn btn-primary btn-sm w-50px"><i class="fa fa-check text-white"></i>
-                                        </a>
-                                    @else
-                                        <a class="btn btn-outline btn-outline-dashed btn-outline-danger btn-sm">
-                                            Leida
-                                        </a>
-                                    @endif
-                                </div>
-
-                            </td>
-                            <td>
-                                <?php
-                                $info = json_decode($n->data);
-                                print_r($info->client);
-                                ?>
-                            </td>
-                            <td> {{ $info->id_cuenta }}</td>
-                            <td>
-                                $3,000.00
-                            </td>
-                            <td>
-                                Caja #333
-                            </td>
-                            <td style="text-align: center">
-                                Pedrito Fernandez
-                            </td>
+        <div class="mx-4">
+            <div class="table-responsive">
+                <table class="data-table-coop table table-hover table-row-dashed fs-6     gy-2 gs-5">
+                    <thead class="thead-dark">
+                        <tr class="fw-semibold fs-3 text-gray-800 border-bottom-2 border-gray-200">
+                            <th class="min-w-150px">Acciones</th>
+                            <th>Id Cuenta</th>
+                            <th>Cliente</th>
+                            <th>Monto transacción (WIP)</th>
+                            <th>Caja (WIP)</th>
+                            <th>Atendido por: (WIP)</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @foreach ($notifications as $n)
+                            <tr>
+                                <td>
+                                    <div class="d-flex">
+                                        @if (!$n->read_at)
+                                            <a n="javascript:void(0);" tol-tip="Marcar como leída" data-offset="20px 20px"
+                                                data-toggle="popover" data-placement="top" data-content="Example content"
+                                                class="btn btn-primary btn-sm w-50px"><i class="fa fa-check text-white"></i>
+                                            </a>
+                                        @else
+                                            <a class="btn btn-outline btn-outline-dashed btn-outline-danger btn-sm">
+                                                Leida
+                                            </a>
+                                        @endif
+                                    </div>
+
+                                </td>
+                                <td>
+                                    <?php
+                                    $info = json_decode($n->data);
+                                    print_r($info->client);
+                                    ?>
+                                </td>
+                                <td> {{ $info->id_cuenta }}</td>
+                                <td>
+                                    $3,000.00
+                                </td>
+                                <td>
+                                    Caja #333
+                                </td>
+                                <td style="text-align: center">
+                                    Pedrito Fernandez
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 
