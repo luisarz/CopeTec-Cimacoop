8439317afdf3982e108dc89fd2b588c389e0e0ce
wom987 <45610462+wom987@users.noreply.github.com>
2023-08-10 21:10:51 -0600

    feat: add csrf_token for ajax globally

--- CopeTec-Cimacoop/resources/views/base/base.blade.php
+++ CopeTec-Cimacoop/resources/views/base/base.blade.php
@@ -36,7 +36,7 @@ License: For each use you must have a valid license purchased only from above li
     <link href=" {{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
     <!--end::Global Stylesheets Bundle-->
-
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 </head>
 <!--end::Head-->
 <!--begin::Body-->
