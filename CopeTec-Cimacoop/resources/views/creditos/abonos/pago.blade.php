a475154b14ccfb10d5082fdb16dfdbbc31c8e257
luisarz <svcomputec@gmail.com>
2023-08-10 23:01:10 -0600

    hidden buttom

--- CopeTec-Cimacoop/resources/views/creditos/abonos/pago.blade.php
+++ CopeTec-Cimacoop/resources/views/creditos/abonos/pago.blade.php
@@ -151,7 +151,8 @@
                 <input type="hidden" name="id_caja" value="{{ $cajaAperturada->id_caja }}">
                 <input type="hidden" name="saldo_capital" id="saldo_capital" value="{{  sprintf("%.2f", $credito->saldo_capital)}}">
                 <input type="hidden" name="aportacion_deposito" id="aportacion_deposito" value="{{ sprintf("%.2f",$credito->aportaciones) }}">
-
+<input type="hidden" step="any" min="{{ $TOTAL_PAGAR }}" value="{{ $TOTAL_PAGAR }}"
+                    name="min_payment" id="min_payment" class="form-control fw-bold text-info" placeholder="Monto Abonar">
 
                 <div class="form-group row mb-5">
                     <div class="form-floating fv-row col-lg-4">
