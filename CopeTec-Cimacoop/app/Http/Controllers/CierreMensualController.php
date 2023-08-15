f3e7aeb58262e34538af47f89faec0dfbd332ef3
luisarz <svcomputec@gmail.com>
2023-08-15 08:43:46 -0600

    Cierre mensual en proces, busca cierre anterior, para obtener el saldo inicial, buscamo movimientos para calcular saldo del mes

--- CopeTec-Cimacoop/app/Http/Controllers/CierreMensualController.php
+++ CopeTec-Cimacoop/app/Http/Controllers/CierreMensualController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Catalogo;
 use App\Models\CierreMensualModel;
+use App\Models\CierreMensualPartidaModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,10 +12,10 @@ class CierreMensualController extends Controller
 {
     public function index(Request $request)
     {
-        
+
         $filtro = $request->input('filtro');
         $cierres = CierreMensualModel::paginate(10);
-        return view('contabilidad.cierre_mensual.index',compact('cierres','filtro'));
+        return view('contabilidad.cierre_mensual.index', compact('cierres', 'filtro'));
     }
 
     public function add()
@@ -39,6 +41,64 @@ class CierreMensualController extends Controller
             // $anioActual + 1 => $anioActual + 1,
         ];
 
-        return view('contabilidad.cierre_mensual.add',compact('meses','anios'));
+        return view('contabilidad.cierre_mensual.add', compact('meses', 'anios'));
+    }
+
+    public function post(Request $request)
+    {
+
+        $mes_cierre = $request->mes;
+        $anio_cierre = $request->year;
+
+
+        // buscamos si el cierre ya existe
+        $VerificarCierreMes = CierreMensualModel::where('mes', $mes_cierre)
+            ->where('year', $anio_cierre)->first();
+        if ($VerificarCierreMes) {
+            return response()->json(['estado' => 'faild', 'mensaje' => 'El mes que intenta cerrar ya se encuentra cerrado']);
+        }
+
+        //buscamos si existe un cierre anterior
+
+        if ($mes_cierre == 1) {
+            $mes_cierre_anterior = 12;
+            $anio_cierre_anterior = $anio_cierre - 1;
+        } else {
+            $mes_cierre_anterior = $mes_cierre - 1;
+            $anio_cierre_anterior = $anio_cierre;
+        }
+        $cierreAnterior = CierreMensualModel::where('year', $anio_cierre_anterior)
+            ->where('mes', $mes_cierre_anterior)->first();
+
+        $id_cierre_anterior = null;
+        if ($cierreAnterior) {
+            $id_cierre_anterior = $cierreAnterior->id;
+            
+
+        }
+
+
+        $catalogo = Catalogo::all();
+
+        if ($catalogo) {
+            foreach ($catalogo as $cuenta) {
+                $id_cuenta = $cuenta->id;
+                //buscar el saldo del cierre anterior
+                $cierreAnteriorCuenta = CierreMensualPartidaModel::where('cierre_mensual_id', $id_cierre_anterior)
+                    ->where('id_cuenta', $id_cuenta )->get();
+                if ($cierreAnteriorCuenta) {
+                    $saldo_anterior = $cierreAnteriorCuenta->saldo_cierre;
+                } else {
+                    $saldo_anterior = 0;
+                }
+
+                //buscar los saldos de las operaciones del mes
+                
+            }
+
+        }
+
+
+
     }
 }
--- CopeTec-Cimacoop/public/assets/js/app/contabilidad/cerrar-mes.js
+++ CopeTec-Cimacoop/public/assets/js/app/contabilidad/cerrar-mes.js
@@ -1,9 +1,6 @@
 
 $(document).ready(function () {
 
-
-
-
     $("#btnCerrarMes").on("click", function (e) {
         e.preventDefault();
         let mes = $("#mes").val();
@@ -52,180 +49,52 @@ $(document).ready(function () {
             }
         }).then((result) => {
             if (result.isConfirmed) {
-                generarPartidaContable();
+                generarCierre();
             }
         });
-
-
-        let data = {
-            "id_cuenta": id_cuenta,
-            "parcial": parcial,
-            "cargos": cargos,
-            "abonos": abonos,
-            "id_partida": $("#id_partida").val(),
-            "tipo_partida": $("#tipo_partida").val(),
-            "_token": $("#token").val()
-        };
-
-        $.ajax({
-            type: "POST",
-            url: "/contabilidad/partidas-detalle/add",
-            data: data,
-            success: function (response) {
-                // swal.close();
-                let message = response.message;
-                if (response.estado == true) {
-                    swalSuccess("Cuenta agregada", message, "Ok");
-
-                    if ($("#num_partida").val() == "") {
-                        let num_partida = response.numero_partida;
-                        alert(num_partida);
-                        $("#num_partida").val((response.numero_partida != null) ? response.numero_partida : "");
-                    }
-
-                    getPartidaDetalles();
-                    $("#cargos").val(0);
-                    $("#abonos").val(0);
-                    $("#id_cuenta").val("").change();
-                } else {
-                    swalError("Error, al agregar la cuenta", message, "Corregir datos");
-                }
-
-            },
-            error: function (xhr, status, error) {
-                swal.close();
-                console.log(error);
-            },
-            dataType: "json" // Especifica el tipo de datos esperados en la respuesta
-        });
-
-        $("#id_cuenta").val("").change();
-        $("#monto_debe").val(0);
-        $("#monto_haber").val(0);
-
-
     });
 
 
-    $("#btnProcesarPartida").on("click", function (e) {
 
-        e.preventDefault();
+    window.generarCierre = function () {
 
-        let montoDebeText = $("#montoDebe").text();
-        let montoHaberText = $("#montoHaber").text();
-
-        // Remover símbolo "$" y comas de los textos
-        montoDebeText = montoDebeText.replace('$', '').replace(',', '');
-        montoHaberText = montoHaberText.replace('$', '').replace(',', '');
-
-        let montoDebe = parseFloat(montoDebeText);
-        let montoHaber = parseFloat(montoHaberText);
-
-        let fecha_partida = $("#fecha_partida").val();
-        let tipo_partida = $("#tipo_partida").val();
-        let concepto = $("#concepto").val();
-
-        if (fecha_partida == "" || tipo_partida == "" || concepto == "") {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...  ',
-                html: `Los siguientes datos son obligatorios
-                         <ul style="list-style-type: none; text-align: left;">
-                            <li><i class='fa fa-check fs-4 text-danger'></i> <b>Fecha</b></li>
-                            <li><i class='fa fa-check fs-4 text-danger'></i><b>Tipo de Cuenta</b></li>
-                            <li><i class='fa fa-check fs-4 text-danger'></i><b>Concepto</b></li>
-                        </ul>
-                    `,
-                confirmButton: false,
-                cancelButton: true,
-                confirmButtonText: 'Volver y seleccionar datos correctos',
-                customClass: {
-                    confirmButton: "btn btn-danger"
-                }
-
-            });
-            return false;
-        }
-
-
-
-
-        if (montoDebe != montoHaber) {
-            swalError('Oops...', 'Los Saldos no coinciden', "Corregir datos");
-            return false;
-        }
-
-
-
-
-
-
-
-        Swal.fire({
-            icon: 'question',
-            title: 'Generar partida?',
-            html: '¿Estás seguro de que deseas generar la siguiente partida? <br>Cargos <span class=\" badge badge-light-danger fs-5\">  $' + montoDebeText + '</span> Abonos <span class=\" badge badge-light-danger fs-5\">  $' + montoHaberText + '</span>',
-            showCancelButton: true,
-            confirmButtonText: 'Sí, procesar Partida',
-            cancelButtonText: 'Cancelar',
-            allowEscapeKey: false,
-            allowOutsideClick: false,
-            customClass: {
-                confirmButton: "btn btn-danger",
-                cancelButton: "btn btn-info"
-            }
-        }).then((result) => {
-            if (result.isConfirmed) {
-                generarPartidaContable();
-            }
-        });
-
-
-
-
-
-
-
-
-
-
-    });
-
-    window.generarPartidaContable = function () {
-        let id_partida = $("#id_partida").val();
-
+        let token = $('meta[name="csrf-token"]').attr("content")
         let data = {
-            "id_partida": $("#id_partida").val(),
+            "mes": $("#mes").val(),
             "num_partida": $("#num_partida").val(),
-            "yearContable": $("#yearContable").val(),
-            "fecha_partida": $("#fecha_partida").val(),
-            "tipo_partida": $("#tipo_partida").val(),
-            "concepto": $("#concepto").val(),
-            "monto": $("#totalCargo").val(),
-            "_token": $("#token").val()
+            "year": $("#year").val(),
+            "_token": token
         };
 
         $.ajax({
             type: "POST",
-            url: "/contabilidad/partidas/put",
+            url: "/contabilidad/cierre-mensual/cierre",
             data: data, // No es necesario convertir a JSON.stringify
             success: function (response) {
-
+                let mensaje=response.mensaje;
 
                 if (response.estado == "success") {
-
+                    id_cierre = response.id_cierre;
                     Swal.fire({
                         icon: 'success',
-                        title: 'Partida',
-                        text: 'La partida contable fue procesada  exitosamente.',
+                        title: 'Cierre Mensual',
+                        text: mensaje,
                         willClose: () => {
                             // Redirige a la nueva página en una pestaña nueva
-                            window.open('/reportes/partidaContable/' + id_partida, '_blank');
-
-                            // Después de 1 segundo, redirige a otra página en la pestaña actual
+                            window.open('/reportes/partidaContable/' + id_cierre, '_blank');
                             setTimeout(() => {
-                                window.location.href = '/contabilidad/partidas';
+                                window.location.href = '/contabilidad/cierre-mensual';
                             }, 1000);
+                        }
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: mensaje,
+                        confirmButtonText: "Corregir datos",
+                        customClass: {
+                            confirmButton: "btn btn-danger"
                         }
                     });
                 }
@@ -234,86 +103,10 @@ $(document).ready(function () {
                 swal.close();
                 console.log(error);
             },
-            // dataType: "json" // Especifica el tipo de datos esperados en la respuesta
         });
     }
 
 
 
-
-
-    getPartidaDetalles = function () {
-        let id_partida = $("#id_partida").val();
-        $.get("/contabilidad/partidas-detalle/getPartidaDetalles/" + id_partida, function (data) {
-            let tablePartidaDetalles = $("#tablePartidaDetalles");
-            tablePartidaDetalles.empty();
-            let numero = 0;
-            $("#aportacionMonto").val(data.aportacion);
-            $("#liquido").val(data.liquido);
-
-            $.each(data.detalles, function (index, element) {
-
-
-                numero = index + 1;
-                let id_detalle_partida_contable = element.id_detalle_partida_contable;
-                let row = $("<tr>");
-                row.append($("<td style='text-align:center;'>").html(
-                    `<a href="javascript:void(0);" onclick="quitarDescuento(${id_detalle_partida_contable})"><span class='btn btn-sm btn-danger'>Quitar</span></a>`
-                ));
-                row.append($("<td>").text(element.numero));
-                row.append($("<td>").html(element.descripcion));
-
-                row.append($("<td style='text-align:right;' >").html(" $ " + parseFloat(element.cargos).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')));
-
-                row.append($("<td style='text-align:right;'>").text('$ ' + parseFloat(element.abonos).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',')));
-                tablePartidaDetalles.append(row);
-
-            });
-            var formattedMontoDebe = '$' + data.sumCargos;
-
-            $("#totalAbono").val(data.sumAbonos);
-            $("#totalCargo").val(data.sumCargos);
-            var formattedMontoHaber = '$' + data.sumAbonos;
-            $('#montoDebe').text(formattedMontoDebe);
-            $('#montoHaber').text(formattedMontoHaber);
-        });
-
-
-    }
-
-    window.quitarDescuento = function (id) {
-        Swal.fire({
-            text: "Deseas Eliminar este registro",
-            icon: "question",
-            buttonsStyling: false,
-            showCancelButton: true,
-            confirmButtonText: "Si",
-            cancelButtonText: 'No',
-            customClass: {
-                confirmButton: "btn btn-warning",
-                cancelButton: "btn btn-secondary"
-            }
-        }).then((result) => {
-            if (result.isConfirmed) {
-                $.get("/contabilidad/partidas-detalle/delete/" + id, function (data) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Eliminado exitoso',
-                        text: 'El bien  ha sido eliminada correctamente.',
-                        showConfirmButton: false,
-                        timer: 1500,
-                        timerProgressBar: true,
-                        allowOutsideClick: false,
-                        allowEscapeKey: false,
-                        allowEnterKey: false,
-                    });
-
-                    getPartidaDetalles();
-                });
-            }
-        });
-    }
-
-    getPartidaDetalles();
 
 });
