2b2777e86ef8e2c4d5a29fb4d97ed9488f802886
wom987 <45610462+wom987@users.noreply.github.com>
2023-09-25 23:21:47 -0600

    feat: format report done

--- CopeTec-Cimacoop/resources/views/contabilidad/reportes/balanceComprobacion_rep.blade.php
+++ CopeTec-Cimacoop/resources/views/contabilidad/reportes/balanceComprobacion_rep.blade.php
@@ -10,7 +10,11 @@
     <style>
         {{ $estilos }} {{ $stilosBundle }}
     </style>
-
+    <style>
+        .page-break {
+            page-break-after: always !important;
+        }
+    </style>
 </head>
 <?php
 $totalG = 0;
@@ -32,8 +36,8 @@ if (isset($hasta) > 0) {
     </div>
 
     @php
-        $total_cargos = 0;
-        $total_abonos = 0;
+        $total_pasivos_ingresos = 0;
+        $saldo_activos_egresos = 0;
     @endphp
 
     <br>
@@ -57,21 +61,92 @@ if (isset($hasta) > 0) {
 
             {{-- @dd($CuentasContablesPadres); --}}
             @foreach ($formattedParents as $item)
+                @if (substr($item->numero, 0, 1) == 1 || substr($item->numero, 0, 1) == '5')
+                    @if ($item->totalCargos != 0 || $item->totalAbonos != 0 || $item->saldo != 0)
+                        <?php
+                        if (is_null($item->id_cuenta_padre)) {
+                            $saldo_activos_egresos += $item->saldo;
+                        }
+                        ?>
+                        <tr>
+                            <td>{{ $item->numero }}</td>
+                            <td>{{ $item->descripcion }}</td>
+                            <td> @money($item->saldo_anterior) </td>
+                            <td>@money($item->totalCargos)</td>
+                            <td> @money($item->totalAbonos)</td>
+                            <td> @money($item->saldo)</td>
+                        </tr>
+                    @endif
+                @endif
+            @endforeach
+            <tr class="fs-5 border-top py-6 font-bold">
+                <td></td>
+                <td colspan="2" class="text-center"><b>TOTAL ACTIVOS Y EGRESOS</b></td>
+                <td>
+                </td>
+                <td>
+                </td>
+                <td
+                    style="text-align: right; border-bottom: 2px solid rgb(0, 0, 0) !important; border-top: 2px solid rgb(0, 0, 0) !important;
+                border-style: double;">
+                    @money($saldo_activos_egresos)
+
+                </td>
+            </tr>
+
+        </tbody>
+    </table>
+    <div class="page-break"></div>
+    <div class="double-strikethrough text-uppercase">
+        ASOCIACION COOPERATIVA DE AHORRO Y CREDITO LA CIMA - "CIMACOOP, DE R.L." <br>
+        BALANCE DE COMPROBACION
+        <br> AL {{ date('d-', strtotime($hasta)) }}{{ $mes }}{{ date('-Y', strtotime($hasta)) }} <br>
+    </div>
+
+    <div class="double-strikethrough">
+        {{-- {{ $encabezado }} --}}
+    </div>
+
+    <table class="table table-sm fs-7 mb-1 " style="border: 1px solid rgb(255, 255, 255);">
+        <thead style="border-top: 1px solid black; border-bottom: 1px solid black; font-size:18px; "
+            class="font-bold fs-4">
+            <tr style="font-weight:bold">
+                <th style="width: 80px; text-align:rigth;" style="text-align: right;">CUENTA</th>
+                <th style="width: 220px; text-align:left;">DE MAYOR </th>
+                <th style="width: 100px; ">SALDO ANTERIOR
+                </th>
+                <th style="width: 125px; ">CARGOS</th>
+                <th style="width: 125px; ">ABONOS</th>
+                <th style="width: 125px; text-align:right; ">
+                    SALDO ACTUAL</th>
+
+            </tr>
+        </thead>
+        <tbody>
+
+            {{-- @dd($CuentasContablesPadres); --}}
+            @foreach ($formattedParents as $item)
                 <?php
                 $accResult = $item;
                 ?>
-                @if ($item->totalCargos != 0 || $item->totalAbonos != 0 || $item->saldo != 0)
-                    <tr>
-                        <td>{{ $item->numero }}</td>
-                        <td>{{ $item->descripcion }}</td>
-                        <td> @money($item->saldo_anterior) </td>
-                        <td>@money($item->totalCargos)</td>
-                        <td> @money($item->totalAbonos)</td>
-                        <td> @money($item->saldo)</td>
-                    </tr>
-                    
+                @if (substr($item->numero, 0, 1) == 2 || substr($item->numero, 0, 1) == '3' || substr($item->numero, 0, 1) == '4')
+                    @if ($item->totalCargos != 0 || $item->totalAbonos != 0 || $item->saldo != 0)
+                        <?php
+                        if (is_null($item->id_cuenta_padre)) {
+                            $total_pasivos_ingresos += $item->saldo;
+                        }
+                        ?>
+                        <tr>
+                            <td>{{ $item->numero }}</td>
+                            <td>{{ $item->descripcion }}</td>
+                            <td> @money($item->saldo_anterior) </td>
+                            <td>@money($item->totalCargos)</td>
+                            <td> @money($item->totalAbonos)</td>
+                            <td> @money($item->saldo)</td>
+                        </tr>
+                    @endif
                 @endif
-            @endforeach           
+            @endforeach
             <tr class="fs-5 border-top py-6 font-bold">
                 <td></td>
                 <td colspan="2" class="text-center"><b>TOTAL PASIVOS E INGRESOS</b></td>
@@ -82,14 +157,13 @@ if (isset($hasta) > 0) {
                 <td
                     style="text-align: right; border-bottom: 2px solid rgb(0, 0, 0) !important; border-top: 2px solid rgb(0, 0, 0) !important;
                 border-style: double;">
-                    $ {{ number_format($totalG, 2) }}
+                    @money($total_pasivos_ingresos)
 
                 </td>
             </tr>
 
         </tbody>
     </table>
-
 
 </body>
 
