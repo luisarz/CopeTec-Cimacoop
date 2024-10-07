78009d38d9bebff341424168ac7af8de5f073123
luisarz <svcomputec@gmail.com>
2024-10-07 15:32:49 -0600

    Referencias refactor

--- CopeTec-Cimacoop/resources/views/referencias/add.blade.php
+++ CopeTec-Cimacoop/resources/views/referencias/add.blade.php
@@ -3,57 +3,56 @@
 Agregar Cliente
 @endsection
 @section('content')
-    <form action="/referencias/add" method="post" autocomplete="nope">
-        {!! csrf_field() !!}
-        <div class="input-group mb-5"></div>
-          <!--begin::Input group-->
-            <div class="input-group mb-5">
-                <span class="input-group-text" id="basic-addon1">
-                    <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-                </span>
-                <input required type="text" class="form-control" name="nombre" placeholder="nombre" aria-label="nombre" aria-describedby="basic-addon1"/>
-            </div>
-       
-        <!--begin::Input group-->
-            <div class="input-group mb-5">
-                <span class="input-group-text" id="basic-addon1">
-                    <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-                </span>
-                <input required type="text" class="form-control" name="parentesco" placeholder="parentesco" aria-label="parentesco" aria-describedby="basic-addon1"/>
-            </div>
-        <!--begin::Input group-->
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">
-                <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-            </span>
-            <input required type="telf" class="form-control" name="telefono" placeholder="telefono" aria-label="telefono" aria-describedby="basic-addon1"/>
-        </div>
-        <!--begin::Input group-->
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">
-                <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-            </span>
-            <input required type="text" class="form-control" name="direccion" placeholder="direccion" aria-label="direccion" aria-describedby="basic-addon1"/>
-        </div>
-        <!--begin::Input group-->
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">
-                <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-            </span>
-            <input required type="text" class="form-control" name="lugar_trabajo" placeholder="lugar_trabajo" aria-label="lugar_trabajo" aria-describedby="basic-addon1"/>
-        </div>
-        <!--begin::Input group-->
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">
-                <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-            </span>
-            <input required type="text" class="form-control" name="observaciones" placeholder="observaciones" aria-label="observaciones" aria-describedby="basic-addon1"/>
-        </div>
-        
+   <div class="card">
+       <div class="card-header">
+           <div class="card-toolbar">
+               <a href="/referencias" class="btn btn-secondary"><i class="fa-solid fa-arrow-alt-circle-left"></i> Cancelar</a>
+
+           </div>
+           <h3 class="card-title">Agregar Referencia</h3>
+       </div>
+         <div class="card-body">
+             <form action="/referencias/add" method="post" autocomplete="nope">
+                 {!! csrf_field() !!}
+                 <div class="input-group mb-5"></div>
+                 <!--begin::Input group-->
+                 <div class="form-floating mb-5">
+                     <input required type="text" class="form-control" name="nombre" placeholder="nombre" aria-label="nombre" aria-describedby="basic-addon1"/>
+                    <label>Nombre</label>
+                 </div>
+
+                 <!--begin::Input group-->
+                 <div class="form-floating mb-5">
+                     <input required type="text" class="form-control" name="parentesco" placeholder="parentesco" aria-label="parentesco" aria-describedby="basic-addon1"/>
+                    <label>DUI</label>
+                 </div>
+                 <!--begin::Input group-->
+                 <div class="form-floating mb-5">
+                     <input required type="telf" class="form-control" name="telefono" placeholder="telefono" aria-label="telefono" aria-describedby="basic-addon1"/>
+                    <label>Telefono</label>
+                 </div>
+                 <!--begin::Input group-->
+                 <div class="form-floating mb-5">
+                     <input required type="text" class="form-control" name="direccion" placeholder="direccion" aria-label="direccion" aria-describedby="basic-addon1"/>
+                    <label>Dirección</label>
+                 </div>
+                 <!--begin::Input group-->
+                 <div class="form-floating mb-5">
+                     <input required type="text" class="form-control" name="lugar_trabajo" placeholder="lugar_trabajo" aria-label="lugar_trabajo" aria-describedby="basic-addon1"/>
+                    <label>Lugar de Trabajo</label>
+                 </div>
+                 <!--begin::Input group-->
+                 <div class="form-floating mb-5">
+                     <input required type="text" class="form-control" name="observaciones" placeholder="observaciones" aria-label="observaciones" aria-describedby="basic-addon1"/>
+                    <label>Observaciones</label>
+                 </div>
 
 
-        <div class="card-footer d-flex justify-content-end py-6">
-            <button type="submit" class="btn btn-bg-primary btn-text-white">Agregar</button>
-        </div>
-    </form>
+
+                 <div class="card-footer d-flex justify-content-end py-6">
+                     <button type="submit" class="w-100 btn btn-bg-primary btn-text-white">Agregar</button>
+                 </div>
+             </form>
+         </div>
+   </div>
 @endsection
--- CopeTec-Cimacoop/resources/views/referencias/edit.blade.php
+++ CopeTec-Cimacoop/resources/views/referencias/edit.blade.php
@@ -7,56 +7,58 @@ Editar Cliente
 Administracion de Referencias
 @endsection
 @section('content')
-    <form action="/referencias/put" method="post" autocomplete="nope">
-        {!! csrf_field() !!}
-        {{ method_field('PUT') }}
-        <input type="hidden" name="id" value="{{$referencia->id_referencia}}">
-        <div class="input-group mb-5"></div>
-        <!--begin::Input group-->
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">
-                <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-            </span>
-            <input required  value="{{$referencia->nombre}}" type="text" class="form-control" name="nombre" placeholder="nombre" aria-label="Nombre" aria-describedby="basic-addon1"/>
+    <div class="card">
+        <div class="card-header">
+            <div class="card-toolbar">
+                <a href="/referencias" class="btn btn-secondary"><i class="fa-solid fa-arrow-alt-circle-left"></i> Cancelar</a>
+
+            </div>
+            <h3 class="card-title">Editar Referencia</h3>
         </div>
-         <!--begin::Input group-->
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">
-                <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-            </span>
-            <input required  value="{{$referencia->parentesco}}" type="text" class="form-control" name="parentesco" placeholder="parentesco" aria-label="Nombre" aria-describedby="basic-addon1"/>
-        </div>
-         <!--begin::Input group-->
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">
-                <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-            </span>
-            <input required  value="{{$referencia->telefono}}" type="text" class="form-control" name="telefono" placeholder="telefono" aria-label="Nombre" aria-describedby="basic-addon1"/>
-        </div>
-         <!--begin::Input group-->
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">
-                <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-            </span>
-            <input required  value="{{$referencia->direccion}}" type="text" class="form-control" name="direccion" placeholder="direccion" aria-label="dui_cliente" aria-describedby="basic-addon1"/>
-        </div>
-         <!--begin::Input group-->
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">
-                <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-            </span>
-            <input required  value="{{$referencia->lugar_trabajo}}" type="text" class="form-control" name="lugar_trabajo" placeholder="lugar_trabajo" aria-label="dui_extendido" aria-describedby="basic-addon1"/>
-        </div>
-         <!--begin::Input group-->
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">
-                <i class="ki-duotone ki-profile-circle fs-1"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
-            </span>
-            <input required  value="{{$referencia->observaciones}}" type="text" class="form-control" name="observaciones" placeholder="observaciones" aria-label="fecha_expedicion" aria-describedby="basic-addon1"/>
+        <div class="card-body">
+            <form action="/referencias/put" method="post" autocomplete="nope">
+                {!! csrf_field() !!}
+                {{ method_field('PUT') }}
+                <input type="hidden" name="id" value="{{$referencia->id_referencia}}">
+                <div class="form-floating mb-5">
+
+                    <input required  value="{{$referencia->nombre}}" type="text" class="form-control" name="nombre" placeholder="nombre" aria-label="Nombre" aria-describedby="basic-addon1"/>
+                    <label>Nombre</label>
+                </div>
+                <!--begin::Input group-->
+                <div class="form-floating mb-5">
+                    <input required  value="{{$referencia->parentesco}}" type="text" class="form-control" name="parentesco" placeholder="parentesco" aria-label="Nombre" aria-describedby="basic-addon1"/>
+            <label>DUI</label>
+                </div>
+                <!--begin::Input group-->
+                <div class="form-floating mb-5">
+                    <input required  value="{{$referencia->telefono}}" type="text" class="form-control" name="telefono" placeholder="telefono" aria-label="Nombre" aria-describedby="basic-addon1"/>
+                    <label>Telefono</label>
+
+                </div>
+                <!--begin::Input group-->
+                <div class="form-floating mb-5">
+                    <input required  value="{{$referencia->direccion}}" type="text" class="form-control" name="direccion" placeholder="direccion" aria-label="dui_cliente" aria-describedby="basic-addon1"/>
+                    <label>Dirección</label>
+                </div>
+                <!--begin::Input group-->
+                <div class="form-floating mb-5">
+                    <input required  value="{{$referencia->lugar_trabajo}}" type="text" class="form-control" name="lugar_trabajo" placeholder="lugar_trabajo" aria-label="dui_extendido" aria-describedby="basic-addon1"/>
+                    <label>Lugar de Trabajo</label>
+
+                </div>
+                <!--begin::Input group-->
+                <div class="form-floating mb-5">
+                    <input required  value="{{$referencia->observaciones}}" type="text" class="form-control" name="observaciones" placeholder="observaciones" aria-label="fecha_expedicion" aria-describedby="basic-addon1"/>
+                    <label>Observaciones</label>
+                </div>
+
+                <div class="card-footer d-flex justify-content-end py-6">
+                    <button type="submit" class="w-100 btn btn-bg-primary btn-text-white">Modificar</button>
+                </div>
+            </form>
         </div>
 
-        <div class="card-footer d-flex justify-content-end py-6">
-            <button type="submit" class="btn btn-bg-primary btn-text-white">Modificar</button>
-        </div>
-    </form>
+
+    </div>
 @endsection
